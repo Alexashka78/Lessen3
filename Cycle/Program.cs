4ac592c27010adbeb7e4ec0daefe5226c3aff0b4
My Name <myEmail@example.com>
2022-10-15 16:48:41 +0300

    Изучили циклы

---
+++ Cycle/Program.cs
@@ -0,0 +1,14 @@
+﻿// Цикл со счетчиком for
+// for (int i = 2; i <= 10; i++)
+
+// Цикл в цикле
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
